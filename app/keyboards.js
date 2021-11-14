78a9306249cfe2947ff45ede6c10dd97f7d959d9
ilipa <ilya.lipovcev@bk.ru>
2021-11-14 23:59:43 +0300

    navigation stack

--- app/keyboards.js
+++ app/keyboards.js
@@ -1,8 +1,24 @@
-module.exports = mainKeyboard = {
+const backButton = {
+  text: 'Назад',
+  callback_data: 'back',
+};
+
+const mainKeyboard = {
   reply_markup: {
     keyboard: [
-      [{ text: 'Найти игрока', callback_data: 'search_player' }],
-      [{ text: 'Создать игру', callback_data: 'create_game' }],
+      [{ text: 'Игроки', callback_data: 'players' }],
+      [{ text: 'Игры', callback_data: 'games' }],
     ],
   },
 };
+
+const playersKeyboard = {
+  reply_markup: {
+    keyboard: [
+      [backButton, { text: 'Найти игрока', callback_data: 'search_player' }],
+      [{ text: 'Добавить игрока', callback_data: 'create_player' }],
+    ],
+  },
+};
+
+module.exports = { mainKeyboard, playersKeyboard };
--- index.js
+++ index.js
@@ -1,44 +1,71 @@
 require('dotenv').config();
 const MafiaApi = require('./app/api');
-// import mainKeyboard from './app/keyboards';
-const mainKeyboard = require('./app/keyboards');
+const { mainKeyboard, playersKeyboard } = require('./app/keyboards');
 const TelegramBot = require('node-telegram-bot-api');
 
+let navigation = [mainKeyboard];
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 MafiaApi.auth(process.env.API_TOKEN);
 
 bot.setMyCommands([
   { command: '/start', description: 'Запуск приложения' },
   { command: '/players', description: 'Получить список игроков' },
+  { command: '/games', description: 'Работа с играми' },
 ]);
 
-const start = () => {
-  bot.on('callback_query', function onCallbackQuery(callbackQuery) {
-    const action = callbackQuery.data;
-    const msg = callbackQuery.message;
-  });
+bot.on('callback_query', function onCallbackQuery(callbackQuery) {
+  const action = callbackQuery.data;
+  const chatid = callbackQuery.message.chat.id;
 
-  bot.on('message', async (msg) => {
-    const text = msg.text;
-    const chatId = msg.chat.id;
+  console.log('callback');
 
-    if (text === '/start') {
-      return bot.sendMessage(chatId, 'Привет', mainKeyboard);
-    }
-    if (text === '/players') {
-      var players = await MafiaApi.Players.list({ page: 1, perPage: 10 });
-      return bot.sendMessage(
-        chatId,
-        `${players.data[1].nickname}`,
-        mainKeyboard
-      );
-    }
+  if (action === 'players') {
+    getListPlayers(chatid, 1);
+  }
+});
 
-    return bot.sendMessage(chatId, 'Неизвестная команда.', mainKeyboard);
+bot.on('message', async (msg) => {
+  const text = msg.text;
+  const chatId = msg.chat.id;
+  navigation = [...new Set(navigation)];
+
+  if (text === '/start') {
+    return bot.sendMessage(chatId, 'Привет', navigation[navigation.length - 1]);
+  }
+
+  if (text === 'Назад') {
+    navigation.length <= 1 ? navigation : navigation.pop();
+    console.log(navigation);
+    return bot.sendMessage(chatId, '🔙', navigation[navigation.length - 1]);
+  }
+
+  if (text === '/players' || text === 'Игроки') {
+    return getListPlayers(chatId, 1);
+  }
+
+  return bot.sendMessage(
+    chatId,
+    'Неизвестная команда.',
+    navigation[navigation.length - 1]
+  );
+});
+
+const getListPlayers = (chatId, page) => {
+  var playersList = [];
+  navigation.push(playersKeyboard);
+  bot.sendMessage(chatId, `Список игроков:`, navigation[navigation.length - 1]);
+  console.log(navigation);
+
+  MafiaApi.Players.list({ page: page, perPage: 10 }).then((playersData) => {
+    playersData.data.forEach((element) => {
+      playersList.push(element);
+    });
+
+    playersList.forEach((element) => {
+      bot.sendMessage(chatId, element.nickname);
+    });
   });
 };
-
-start();
 
 // bot.onText(/\/echo (.+)/, (msg, match) => {
 //   const chatId = msg.chat.id;
